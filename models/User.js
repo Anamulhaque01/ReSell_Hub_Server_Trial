e9ddb2f2d9118a0f4c08f57b70e0958f51ac7fe8
Anamulhaque01 <anamulhaque0357@gmail.com>
2026-06-27 04:58:20 +0600

    User Model added

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    photo: { type: String, default: "" },
+    role: { type: String, enum: ['buyer', 'seller', 'admin'], default: 'buyer' },
+    phone: { type: String, required: true },
+    location: { type: String, required: true },
+    status: { type: String, enum: ['active', 'blocked'], default: 'active' }
+}, { timestamps: true });
+
+export default mongoose.model('User', userSchema);
